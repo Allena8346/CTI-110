f41588416c6e9b1ce445dc220bc87ce0d793a537
Allena8346 <allena8346@student.faytechcc.edu>
2024-09-16 11:22:37 -0400

    Add files via upload

---
+++ P1LAB1_AllenAlexander.py
@@ -0,0 +1,8 @@
+#Alexander Allen
+#16Sep24
+#P1Lab1
+#Get input from user and displays output
+
+firstName = input('Enter your first name: ')
+lastName = input('Enter your last name: ')
+print('Hello,', firstName, lastName+'!', ' Welcome to CTI-110')
