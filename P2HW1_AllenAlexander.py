9fb69d545840c36fa32c6105f4cb9e023c52fa76
Allena8346 <allena8346@student.faytechcc.edu>
2024-09-30 11:14:10 -0400

    Add files via upload

---
+++ P2HW1_AllenAlexander.py
@@ -0,0 +1,28 @@
+#Alexander Allen
+#30Sep24
+#P2HW1
+#Using imported library, math, and f-string
+
+# Import math library
+import math
+
+#print() makes an empty print line or adding '\n' to the begining or end to get an empty line above or below
+
+#Variables
+radius = float(input('What is the radius of the circle? '))
+x = math.pi
+diameter = 2 * radius
+
+#Display diameter with one decimal place.
+print('\n'f'The diameter of the circle is {diameter:.1f}''\n')
+circumference = 2 * x * radius
+print (f'The circumference of the circle is {circumference:.2f}''\n')
+area = x * math.pow(radius, 2)
+print (f'The area of the circle is {area:.3f}')
+
+#Prints calculated information
+
+#print ('\n''The diameter of the circle is', round(diameter,1),'\n')
+#print ('The circumference of the circle is', round(circumference,2),'\n')
+#print ('The area of the circle is', round(area,3))
+
